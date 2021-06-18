d612a25e7e2ace8d2d68c7fdabf3ff0767e8a31e
Diego Henrique Silva Oliveira <53708053+DiegoHSO@users.noreply.github.com>
2021-06-18 17:59:42 -0300

    begun library screen layout

---
+++ Music Service/Music Service/LibraryViewController.swift
@@ -0,0 +1,66 @@
+//
+//  LibraryViewController.swift
+//  Music Service
+//
+//  Created by Diego Henrique on 18/06/21.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+
+    // cell ID: library-detail
+    
+    private var musicService: MusicService?
+    private var collections: [MusicCollection] = []
+    
+    @IBOutlet weak var libraryTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        do {
+            self.musicService = try MusicService()
+            self.collections = musicService?.loadLibrary() ?? []
+        } catch {
+            print(error)
+        }
+        libraryTableView.dataSource = self
+        libraryTableView.delegate = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return collections.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "library-detail", for: indexPath)
+        
+        let musicItem = collections[indexPath.row]
+        
+        cell.textLabel?.text = musicItem.title
+        cell.detailTextLabel?.text = musicItem.type == .album ?
+            "Album · \(musicItem.mainPerson)" :
+            "Playlist · \(musicItem.mainPerson)"
+        
+        cell.imageView?.image = UIImage(named: musicItem.id)
+        
+        return cell
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
